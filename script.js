7d822ebae2ca26e1c695d98bbc9e9e0cab728fcd
the-flash-barryallen <nortoyevotabek420@gmail.com>
2023-05-19 10:22:48 +0500

    first comment

---
+++ script.js
@@ -0,0 +1,7 @@
+let age=prompt("tell your age")
+if(age>8 && age>18){
+    console.log("you're rigth you can be there")
+}
+else{
+    console.log("you're are too old either too young that's why you cna work there")
+}
